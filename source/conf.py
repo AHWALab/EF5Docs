2b40f7b0d5f12b0486bdb5ad1975f79c4d3dff27
naman <namanmehta1234@gmail.com>
2025-02-21 13:53:08 -0600

    Logo addded on the page.

--- source/conf.py
+++ source/conf.py
@@ -25,4 +25,11 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    "style_nav_header_background": "#003285",  
+
+}
+
 html_static_path = ['_static']
+
+html_logo = '_static/EF5-01.png'
